e38075e660c7aa79d0326733ef6c042e678cc752
kasperstorgaard <kasper.storgaard@gmail.com>
2022-03-01 22:42:52 +0100

    feat(components): add lightbox component to showcase images/items in a scrollable modal view

---
+++ src/components/lightbox/lightbox.stories.ts
@@ -0,0 +1,84 @@
+import { html, nothing, render, TemplateResult } from 'lit';
+import './lightbox';
+
+export default {
+  title: 'Lightbox',
+};
+
+interface Options {
+  isOpen?: boolean;
+  direction?: 'up'|'down';
+}
+
+interface Content {
+  outer?: TemplateResult;
+  inner?: TemplateResult;
+  header?: TemplateResult;
+  footer?: TemplateResult;
+}
+
+function lightboxFactory(options?: Options, content?: Content) {
+  const fn = (args?: Options) => {
+    const container = document.createElement('div');
+
+    const template = html`
+    <button
+      class="sg-button"
+      @click=${() => document.querySelector('sg-lightbox')?.setAttribute('is-open', '')}
+    >open</button>
+    ${content?.outer ?? nothing}
+    <sg-lightbox ?is-open=${args.isOpen} direction=${args.direction ?? 'up'}>
+      ${content?.inner ?? html`
+        ${getImage()}
+        ${getImage()}
+        ${getImage()}
+        ${getImage()}
+      `}
+    </sg-lightbox>
+    `
+
+    render(template, container);
+    return container;
+  }
+
+  fn.args = options;
+  fn.argTypes = {
+    ...fn.argTypes,
+    direction: {
+      options: ['up', 'down'],
+      control: 'select',
+      defaultValue: 'up',
+    },
+  };
+
+  return fn;
+}
+
+function getImage(width = 1200, height = 800) {
+  const cacheBust = Math.floor(Math.random() * 10000);
+  return html`<img src="https://picsum.photos/${width}/${height}?_cb=${cacheBust}">`;
+}
+
+export const Primary = lightboxFactory({ isOpen: false });
+export const OddSizes = lightboxFactory({ isOpen: false }, {
+  inner: html`
+  ${getImage(400, 400)}
+  ${getImage(650, 100)}
+  ${getImage(450, 550)}
+  ${getImage(1200, 800)}
+  ${getImage(50, 50)}
+  ${getImage(300, 200)}
+  `
+});
+export const PreOpened = lightboxFactory({ isOpen: true });
+export const Down = lightboxFactory({ direction: 'down', isOpen: false });
+
+export const UseScrollLock = lightboxFactory(
+  { isOpen: false},
+  { outer: html`
+    <div style="height: 300vh; background: var(--gradient-1); padding: var(--size-5); color: var(--gray-1)">
+      try to scroll while closed
+    </div>`,
+  }
+);
+
---
+++ src/components/lightbox/lightbox.ts
@@ -0,0 +1,264 @@
+import { css, html, LitElement, PropertyValues } from 'lit';
+
+import { customElement, property, query } from 'lit/decorators.js';
+import { Overlayable } from '../overlay/overlayable-mixin';
+import { ScrollController } from '../scroll/scroll-controller';
+import '../overlay/overlay';
+
+@customElement('sg-lightbox')
+export class LightboxElement extends Overlayable(LitElement) {
+  static styles = [getStyles()];
+
+  private _scroll: ScrollController<LightboxElement>
+
+  constructor(
+  ) {
+    super();
+    this._scroll = new ScrollController(this);
+  }
+
+  @query('.container')
+  scrollContainer: HTMLElement | null;
+
+  @property({ type: String, reflect: true })
+  dir: 'up'|'down' = 'up';
+
+  @property({ type: Boolean, reflect: true, attribute: 'animates' })
+  animates = false;
+
+  @property({ type: String })
+  closeLabel: string = 'close';
+
+  @property({ type: String })
+  backLabel: string = 'back';
+
+  @property({ type: String })
+  forwardLabel: string = 'forward';
+
+  render() {
+    console.log(this._scroll.isAtEnd);
+    return html`
+    <div class="container">
+      <slot
+        @click="${(event: Event) => event.stopPropagation()}"
+      ></slot>
+    </div>
+    <button
+      class="close"
+      @click=${this.close}
+      aria-label=${this.closeLabel}
+    >&#x2715</button>
+    <button
+      class="arrow-back"
+      aria-label=${this.backLabel}
+      ?hidden=${this._scroll.isAtStart}
+      @click=${this._backHandler}
+    >
+      <slot name="arrow-back">
+        &#x2039
+      </slot>
+    </button>
+    <button
+      class="arrow-forward"
+      aria-label=${this.forwardLabel}
+      ?hidden=${this._scroll.isAtEnd}
+      @click=${this._forwardHandler}
+    >
+      <slot name="arrow-forward">
+        &#x203A
+      </slot>
+    </button>
+    `;
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    this.addEventListener('click', () => this.close());
+    this.addEventListener('animationend', () => this.animates = false);
+    this.addEventListener('animationcancel', () => this.animates = false);
+
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'complementary');
+    }
+  }
+
+  updated(props: PropertyValues) {
+    super.updated(props)
+
+    if (props.has('isOpen') && props.get('isOpen') != null) {
+      this.animates = true;
+    }
+  }
+
+  open() {
+    this.isOpen = true;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+  private _backHandler(event: Event) {
+    event.stopPropagation();
+    this._scroll.goBack();
+  }
+
+  private _forwardHandler(event: Event) {
+    event.stopPropagation();
+    this._scroll.goForward();
+  }
+}
+
+function getStyles() {
+  return css`
+  [hidden] {
+    display: none !important;
+  }
+
+  :host {
+    --lightbox-width: min(42rem, 100vw);
+    --lightbox-height: min(33rem, 100vh);
+    --lightbox-arrow-size: var(--size-7);
+    --lightbox-arrow-distance: var(--app-gutter);
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+
+    display: grid;
+    grid-template-columns: minmax(0, auto) var(--lightbox-width) minmax(0, auto);
+    grid-template-rows: minmax(0, auto) var(--lightbox-height) minmax(0, auto);
+    grid-template-areas:
+      ".     .     ."
+      ". container ."
+      ".     .     .";
+    align-items: center;
+
+    z-index: var(--level-modal);
+  }
+
+  .close {
+    position: absolute;
+    top: 0;
+    right: 0;
+
+    padding: var(--lightbox-close-pad, var(--size-3) var(--app-gutter));
+
+    font-size: var(--font-size-2);
+    line-height: 1.5em;
+
+    background: none;
+    outline: none;
+    border: none;
+
+    transition: transform .33s var(--ease-3);
+  }
+
+  .arrow-back,
+  .arrow-forward {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: fixed;
+    top: var(--lightbox-arrow-top, 50%);
+
+    background: var(--lightbox-arrow-background, var(--gray-1));
+    width: var(--lightbox-arrow-width, var(--lightbox-arrow-size));
+    height: var(--lightbox-arrow-width, var(--lightbox-arrow-size));
+
+    transform: var(--lightbox-arrow-transform, translateY(-50%));
+
+    font-size: var(--lightbox-arrow-font-size, var(--font-size-fluid-1));
+
+    box-shadow: var(--shadow-3);
+
+    outline: none;
+    border: none;
+  }
+
+  .arrow-back {
+    left: var(--lightbox-arrow-left, var(--lightbox-arrow-distance));
+  }
+
+  .arrow-forward {
+    right: var(--lightbox-arrow-right, var(--lightbox-arrow-distance));
+  }
+
+  .container {
+    grid-area: container;
+
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: 100%;
+    grid-template-rows: 100%;
+    align-items: var(--lightbox-align, center);
+    align-content: center;
+    justify-items: center;
+
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    -ms-overflow-style: none;
+    scrollbar-width: none; /* Firefox */
+
+    background: var(--sidebar-bg, transparent);
+  }
+
+  .container::-webkit-scrollbar {
+    display: none;
+  }
+
+  .container slot::slotted(*) {
+    scroll-snap-align: center;
+    max-height: 100%;
+    max-height: 100vh;
+    overflow-y: scroll;
+  }
+
+  /* hide when not open OR animating */
+  :host(:not([is-open], [animates])) {
+    display: none;
+  }
+
+  :host([direction=up][animates]) {
+    transform-origin: left center;
+  }
+
+  :host([direction=up][is-open][animates]) {
+    animation:
+      var(--animation-fade-in),
+      var(--animation-slide-in-up);
+  }
+
+  :host([direction=up]:not([is-open])[animates]) {
+    animation:
+      var(--animation-fade-out),
+      var(--animation-slide-out-down);
+  }
+
+  :host([direction=up][animates]) {
+    transform-origin: top center;
+  }
+
+  :host([direction=down][is-open][animates]) {
+    animation:
+      var(--animation-fade-in),
+      var(--animation-slide-in-down);
+  }
+
+  :host([direction=down]:not([is-open])[animates]) {
+    animation:
+      var(--animation-fade-out),
+      var(--animation-slide-out-up);
+  }
+  `;
+}

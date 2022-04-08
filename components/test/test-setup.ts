8a8e725086ba8633860a552caa685c136b97833a
kasperstorgaard <kasper.storgaard@gmail.com>
2022-04-08 23:33:51 +0200

    test: cleanup test setup utils

--- components/test/test-setup.ts
+++ components/test/test-setup.ts
@@ -1,53 +1,5 @@
-import { devices, BrowserContextOptions } from 'playwright';
 import { test as base } from '@playwright/test';
 import { DesignSystemPage } from './design-system-page';
-
-export type BrowserKey = 'chromium'|'webkit'|'firefox'|'chrome'|'edge'|'safari';
-export type DeviceKey = 'iphone'|'desktop'|'android';
-
-export interface DeviceConfig {
-  browser: BrowserKey,
-  options: BrowserContextOptions,
-}
-
-export const DEVICES = {
-  iphone: {
-    name: 'iphone',
-    browser: 'webkit',
-    options: { ...devices['iPhone 8'] },
-  },
-  android: {
-    name: 'android',
-    browser: 'chrome',
-    options: { ...devices['Pixel 2'] },
-  },
-  desktop: {
-    name: 'desktop',
-    browser: 'chrome',
-    options: {
-      viewport: {
-        width: 1440,
-        height: 900,
-      },
-    },
-  },
-};
-
-export const DEFAULT_DEVICE = DEVICES.android;
-
-const deviceOverride = process.env.DEVICE?.toLowerCase() as DeviceKey;
-const device = DEVICES[deviceOverride] ?? DEFAULT_DEVICE;
-
-const headless = process.env.HEADLESS !== 'false';
-const baseUrl = process.env.BASE_URL ?? 'http://localhost:6006';
-
-export const env = {
-  device,
-  headless,
-  baseUrl,
-  isDesktop: device.name === 'desktop',
-  isMobile: device.name !== 'desktop',
-};
 
 // Declare the types of your fixtures.
 type MyFixtures = {
@@ -58,7 +10,7 @@ type MyFixtures = {
 // This new "test" can be used in multiple test files, and each of them will get the fixtures.
 export const test = base.extend<MyFixtures>({
   dsPage: async ({ page }, use) => {
-    await use(new DesignSystemPage(page, env.baseUrl));
+    await use(new DesignSystemPage(page));
   },
 });
 

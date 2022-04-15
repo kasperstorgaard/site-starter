fe232f3e7fc5030c1fe1cae79a26aeeec18caac3
kasperstorgaard <kasper.storgaard@gmail.com>
2022-04-15 08:38:48 +0200

    test(components): rework design system page utility setup

--- components/test/design-system-page.ts
+++ components/test/design-system-page.ts
@@ -6,12 +6,11 @@ export class DesignSystemPage {
 
   constructor(page: Page) {
     this.page = page;
-
-    // blocks any netlify tooling, so they don't interfere with tests.
-    page.route('**/netlify.js', route => route.abort('blockedbyclient'));
   }
 
   async goto(type: 'atom'|'molecule'|'organism', name: string, variant?: string) {
+    await this._blockNetlifyScripts();
+
     const basePath = '/iframe.html';
 
     const idPath = variant ?
@@ -22,5 +21,10 @@ export class DesignSystemPage {
     await this.page.locator('#root > div').waitFor();
 
     return this.page;
+  }
+
+  private async _blockNetlifyScripts() {
+    // blocks any netlify tooling, so they don't interfere with tests.
+    await this.page.route('**/netlify.js', route => route.abort('blockedbyclient'));
   }
 }

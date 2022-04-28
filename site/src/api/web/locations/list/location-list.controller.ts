99bd4c34f7c9843feec41f649945644b60a8f3df
kasperstorgaard <kasper.storgaard@gmail.com>
2022-04-28 17:25:29 +0200

    refactor(site/api): rework locations-list controller to use relative api url

--- site/src/api/web/locations/list/location-list.controller.ts
+++ site/src/api/web/locations/list/location-list.controller.ts
@@ -1,11 +1,5 @@
 import { ReactiveController, LitElement } from 'lit';
 
-// TODO: export to shared
-interface ApiConfig {
-  port: number,
-  isDev: boolean,
-}
-
 export interface LocationItem {
   id: string,
   created_at: string,
@@ -15,25 +9,15 @@ export interface LocationItem {
 
 export class LocationsListController<T extends LitElement = any> implements ReactiveController {
   private host: T | null = null;
-  private baseUrl: URL;
-  private apiConfig: ApiConfig
 
   public data: LocationItem[] | null = null;
   public error: Error | null = null;
   public state: 'ready'|'loading'|'error'|'initial' = 'initial';
 
   // TODO: extract to shared api controller configs,
-  constructor(host: T | null, apiConfig: {
-    port: number,
-    isDev: boolean,
-  }) {
+  constructor(host: T | null) {
     this.host = host;
     this.host?.addController(this);
-    this.apiConfig = apiConfig;
-
-    this.baseUrl = new URL(window.location.href);
-    this.baseUrl.port = apiConfig.port.toString();
-    this.baseUrl.pathname = '/api/locations';
   }
 
   hostConnected(): void {
@@ -46,14 +30,13 @@ export class LocationsListController<T extends LitElement = any> implements Reac
 
     // TODO: add filtering
     try {
-      const url = new URL(this.baseUrl);
-
-      const response = await fetch(url.href);
+      const response = await fetch('/api/locations');
 
       if (!response.ok) {
         // TODO: implement ApiError which unwraps the endpoint error
         throw new Error("Unable to get locations")
       }
+
       this.data = await response.json();
       this.state = 'ready';
       return this.data;
